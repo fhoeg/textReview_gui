686eba1e05af07ba7bc3d641078f5ea920dc493c
fhoeg <felixhoegner@fhoeg.de>
2023-10-20 09:23:34 +0200

    max tokens and docs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def chat_with_model(prompt, guidelines):
     ]
     response = openai.ChatCompletion.create(
         model="gpt-4",
-        max_tokens=1000,
+        max_tokens=1536,
         messages=messages,
         temperature=0
     )
@@ -25,11 +25,11 @@ def chat_with_model(prompt, guidelines):
 # Streamlit App
 st.title("OpenAI Chat Assistant for Text Improvement")
 
-uploaded_file = st.file_uploader("Upload a text file containing content guidelines", type="txt")
+uploaded_file = st.file_uploader("Bitte laden Sie eine Text-Datei mit Ihren Guidelines hoch.", type="txt")
 
 if uploaded_file is not None:
     guidelines = uploaded_file.read().decode("utf-8")
-    st.write("Content Guidelines Loaded. Proceed to the chat.")
+    st.write("Content Guidelines Geladen. Bitte geben Sie Ihr Textbeispiel ein.")
     
     user_input = st.text_area("You: ")
     
@@ -37,4 +37,4 @@ if uploaded_file is not None:
         assistant_reply = chat_with_model(user_input, guidelines)
         st.write(f"Assistant: {assistant_reply}")
 else:
-    st.write("Please upload a text file to proceed.")
+    st.write("Bitte laden Sie eine Textdatei hoch um fortzufahren.")
